79db8a294b139deacac90dfbc18e2dd35b1e4db1
JAEGEUN SONG <76800974+drow724@users.noreply.github.com>
2021-08-04 21:41:53 +0900

    Add files via upload

---
+++ error-catch/src/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
+import App from "./App";
+
+Sentry.init({
+  dsn: "https://8ad8e40af5c64aff9b57dec828a75f5b@o943791.ingest.sentry.io/5892740",
+  integrations: [new Integrations.BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+// Can also use with React Concurrent Mode
+// ReactDOM.createRoot(document.getElementById('root')).render(<App />);
